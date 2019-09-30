126be88eda9d7d25c28d98e04a5a50510639e5c8
antessio <antessio7@gmail.com>
2019-10-01 00:45:24 +0200

    Documentation + authorizer as request + index and entity dynamic

--- serverless/dynamoplus/http/authorizer.py
+++ serverless/dynamoplus/http/authorizer.py
@@ -12,13 +12,14 @@ AUTH0_CLIENT_PUBLIC_KEY = os.getenv('AUTH0_CLIENT_PUBLIC_KEY')
 AUDIENCE = os.getenv('AUDIENCE')
 
 def authorize(event, context):
-    whole_auth_token = event.get('authorizationToken')
+    whole_auth_token = event["headers"]["Authorization"]
     if not whole_auth_token:
         raise Exception('Unauthorized')
 
     print('Client token: ' + whole_auth_token)
     print('Method ARN: ' + event['methodArn'])
-
+    entity = event["pathParameters"]["entity"]
+    print("Entity {}".format(entity))
     token_parts = whole_auth_token.split(' ')
     auth_token = token_parts[1]
     token_method = token_parts[0]
@@ -28,19 +29,19 @@ def authorize(event, context):
         raise Exception('Unauthorized')
 
     try:
-        principal_id = jwt_verify(auth_token, AUTH0_CLIENT_PUBLIC_KEY)
+        payload = jwt_verify(auth_token, AUTH0_CLIENT_PUBLIC_KEY)
+        print("Auth0 payload "+str(payload))
+        principal_id = payload['sub']
         policy = generate_policy(principal_id, 'Allow', event['methodArn'])
         return policy
     except Exception as e:
         print(f'Exception encountered: {e}')
         raise Exception('Unauthorized')
-
-def jwt_verify(auth_token, public_key):
+def jwt_verify(auth_token,public_key):
     public_key = format_public_key(public_key)
     pub_key = convert_certificate_to_pem(public_key)
     payload = jwt.decode(auth_token, pub_key, algorithms=['RS256'], audience=AUDIENCE)
-    return payload['sub']
-
+    return payload
 
 def generate_policy(principal_id, effect, resource):
     return {
--- serverless/dynamoplus/http/query.py
+++ serverless/dynamoplus/http/query.py
@@ -4,7 +4,7 @@ logging.basicConfig(level=logging.INFO)
 from dynamoplus.http.handler import HttpHandler
 
 def query(event, context):
-    entities = os.environ['entities']
+    entities = os.environ['ENTITIES']
     dynamodbTable = os.environ['DYNAMODB_TABLE']
     handler = HttpHandler(entities,dynamodbTable)
     return handler.query(event['pathParameters'],queryStringParameters=event['queryStringParameters'],body=event["body"],headers=event['headers'])
