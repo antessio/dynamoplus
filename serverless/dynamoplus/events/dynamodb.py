12ad1ee950f611100cd198b1aef8d48dbfe04930
antessio <antessio7@gmail.com>
2019-10-01 23:15:45 +0200

    Refactoring duplicated code

--- serverless/dynamoplus/events/dynamodb.py
+++ serverless/dynamoplus/events/dynamodb.py
@@ -26,8 +26,8 @@ def dynamoStreamHandler(event, context):
     indexes = os.environ['INDEXES'].split(",")
     entities = os.environ['ENTITIES'].split(",")
     logger.info("Events on dynamo {} ".format(str(event)))
-    systemEntitiesIndexService = IndexService(tableName, "entity", "entity#name",dynamodb)
-    systemIndexesIndexService = IndexService(tableName, "index", "index#entity.name",dynamodb)
+    systemDocumentTypesIndexService = IndexService(tableName, "entity", "document_type#name",dynamodb)
+    systemIndexesIndexService = IndexService(tableName, "index", "index#document_type.name",dynamodb)
     for record in event.get('Records'):
         keys = record['dynamodb']['Keys']
         
@@ -35,114 +35,78 @@ def dynamoStreamHandler(event, context):
         sk = keys['sk']['S']
         indexUtils = IndexUtils()
         if "#" not in sk:
-            entityConfiguration = None
-            logger.info("Find entity by name {}".format(sk))
-            systemEntitiesResult = systemEntitiesIndexService.findByExample({"name": sk})
-            logger.info("Response is {}".format(str(systemEntitiesResult)))
-            if "data" in systemEntitiesResult:
-                if len(systemEntitiesResult["data"])>0:
-                    entityConfiguration = systemEntitiesResult["data"][0]
+            documentConfiguration = None
+            logger.info("Find documentType by name {}".format(sk))
+            documentTypesResult = systemDocumentTypesIndexService.findByExample({"name": sk})
+            logger.info("Response is {}".format(str(documentTypesResult)))
+            if "data" in documentTypesResult:
+                if len(documentTypesResult["data"])>0:
+                    documentConfiguration = documentTypesResult["data"][0]
                 
             if record.get('eventName') == 'INSERT':
                 newRecord = deserialize(record['dynamodb']['NewImage'])
-                if entityConfiguration:
-                    entity=entityConfiguration["name"]
-                    idKey = entityConfiguration["idKey"]
-                    customIndexes =  None
-                    logger.info("Search custom indexes for entity {}".format(entity))
-                    customIndexesResult = systemIndexesIndexService.findByExample({"entity":{"name": entity}})
-                    if customIndexesResult:
-                        if "data" in customIndexesResult:
-                            logger.info("Found {} for entity {}".format(len(customIndexesResult["data"]),entity))
-                            if len(customIndexesResult["data"])>0:
-                                customIndexes= ",".join(map(lambda i: i["entity"]["name"]+"#"+i["name"], customIndexesResult["data"]))
-                    if customIndexes:
-                        logger.info("Custom indexes string {}".format(customIndexes))
-                        customMatchingIndexes = indexUtils.findIndexFromEntity(customIndexes,newRecord,sk)
-                        for i in customMatchingIndexes.keys():
-                            try:
-                                logger.info("index found {}".format(i))
-                                index = customMatchingIndexes[i]
-                                logger.info("index tablePrefix {} and conditions {}".format(index["tablePrefix"], index["conditions"]))
-                                indexKeys = index["conditions"]
-                                logger.info("Index keys {}".format(indexKeys))
-                                orderingKey = index["orderBy"] if "orderBy" in index else None
-                                logger.info("Entity {} idKey {} orderingKey {}".format(entity,idKey, orderingKey))
-                                repository =  IndexRepository(tableName,index["tablePrefix"],idKey,orderingKey,indexKeys,dynamoDB=dynamodb)
-                                entity = dict(filter(lambda kv: kv[0] not in ["geokey","hashkey"], newRecord.items()))
-                                logger.info("creating index for {}".format(str(entity)))
-                                repository.create(entity)
-                            except Exception as e:
-                                logger.warning("Unable to create the index {}".format(i),exc_info=e)
-
-                    logger.info("System indexes string {}".format(indexes))
-                    systemMatchingIndexes = indexUtils.findIndexFromEntity(indexes,newRecord,sk)
-                    logger.info("Found {} indexes matching the record for system indexes".format(len(systemMatchingIndexes)))
-                    for i in systemMatchingIndexes.keys():
-                        try:
-                            logger.info("index found {}".format(i))
-                            index = systemMatchingIndexes[i]
-                            logger.info("index tablePrefix {} and conditions {}".format(index["tablePrefix"], index["conditions"]))
-                            indexKeys = index["conditions"]
-                            logger.info("Index keys {}".format(indexKeys))
-                            orderingKey = index["orderBy"] if "orderBy" in index else None
-                            logger.info("Entity {} idKey {} orderingKey {}".format(entity,idKey, orderingKey))
-                            repository =  IndexRepository(tableName,index["tablePrefix"],idKey,orderingKey,indexKeys,dynamoDB=dynamodb)
-                            entity = dict(filter(lambda kv: kv[0] not in ["geokey","hashkey"], newRecord.items()))
-                            logger.info("creating index for {}".format(str(entity)))
-                            repository.create(entity)
-                        except Exception as e:
-                            logger.warning("Unable to create the index {}".format(i),exc_info=e)
-                else:
-                    logger.info("Unable to retrieve the entity {} ".format(sk))
+                document = dict(filter(lambda kv: kv[0] not in ["geokey","hashkey"], newRecord.items()))
+                logger.info("creating index for {}".format(str(document)))
+                handleIndexes(lambda repository,idKey: repository.create(document), documentConfiguration, systemIndexesIndexService, indexUtils, newRecord, sk, tableName, indexes)
                 
             elif record.get('eventName') == 'MODIFY':
                 newRecord = deserialize(record['dynamodb']['NewImage'])
-                oldRecord = deserialize(record['dynamodb']['OldImage'])
-                if entityConfiguration:
-                    systemMatchingIndexes = indexUtils.findIndexFromEntity(indexes,newRecord,sk)
-                    for i in systemMatchingIndexes.keys():
-                        try:
-                            logger.info("index found {}".format(i))
-                            index = systemMatchingIndexes[i]
-                            logger.info("index tablePrefix {} and conditions {}".format(index["tablePrefix"], index["conditions"]))
-                            indexKeys = index["conditions"]
-                            logger.info("Index keys {}".format(indexKeys))
-                            entity=entityConfiguration["name"]
-                            idKey = entityConfiguration["idKey"]
-                            orderingKey = index["orderBy"] if "orderBy" in index else None
-                            logger.info("Entity {} idKey {} orderingKey {}".format(entity,idKey, orderingKey))
-                            repository =  IndexRepository(tableName,index["tablePrefix"],idKey,orderingKey,indexKeys,dynamoDB=dynamodb)
-                            entity = dict(filter(lambda kv: kv[0] not in ["geokey","hashkey"], newRecord.items()))
-                            logger.info("updating index for {}".format(str(entity)))
-                            repository.update(entity)
-                        except Exception as e:
-                            logger.warning("Unable to create the index {}".format(i),exc_info=e)
-                else:
-                    logger.info("Unable to retrieve the entity {} ".format(sk))
+                document = dict(filter(lambda kv: kv[0] not in ["geokey","hashkey"], newRecord.items()))
+                logger.info("updating index for {}".format(str(document)))
+                handleIndexes(lambda repository,idKey: repository.update(document),documentConfiguration, systemDocumentTypesIndexService, indexUtils, newRecord, sk, tableName,indexes)
                 
             elif record.get('eventName') == 'REMOVE':
                 oldRecord = deserialize(record['dynamodb']['OldImage'])
-                if entityConfiguration:
-                    systemMatchingIndexes = indexUtils.findIndexFromEntity(indexes,oldRecord,sk)
-                    for i in systemMatchingIndexes.keys():
-                        try:
-                            logger.info("index found {}".format(i))
-                            index = systemMatchingIndexes[i]
-                            logger.info("index tablePrefix {} and conditions {}".format(index["tablePrefix"], index["conditions"]))
-                            indexKeys = index["conditions"]
-                            logger.info("Index keys {}".format(indexKeys))
-                            entity=entityConfiguration["name"]
-                            idKey = entityConfiguration["idKey"]
-                            orderingKey = index["orderBy"] if "orderBy" in index else None
-                            logger.info("Entity {} idKey {} orderingKey {}".format(entity,idKey, orderingKey))
-                            repository =  IndexRepository(tableName,index["tablePrefix"],idKey,orderingKey,indexKeys,dynamoDB=dynamodb)
-                            entity = dict(filter(lambda kv: kv[0] not in ["geokey","hashkey"], oldRecord.items()))
-                            logger.info('removing index on record  {}'.format(pk))
-                            repository.delete(oldRecord[idKey])
-                        except Exception as e:
-                            logger.warning("Unable to create the index {}".format(i),exc_info=e)
-                else:
-                    logger.info("Unable to retrieve the entity {} ".format(sk))
+                document = dict(filter(lambda kv: kv[0] not in ["geokey","hashkey"], oldRecord.items()))
+                logger.info('removing index on record  {}'.format(pk))
+                handleIndexes(lambda repository,idKey : repository.delete(oldRecord[idKey]),documentConfiguration, systemDocumentTypesIndexService, indexUtils, oldRecord, sk, tableName,indexes)
         else:
             logger.debug('Skipping indexing on record {} - {}'.format(pk,sk))
+
+def handleIndexes(repositoryLambda, documentConfiguration, systemIndexesIndexService, indexUtils, newRecord, sk, tableName, indexes):
+    if documentConfiguration:
+        documentTypeName=documentConfiguration["name"]
+        idKey = documentConfiguration["idKey"]
+        customIndexes =  None
+        logger.info("Search custom indexes for document {}".format(documentTypeName))
+        customIndexesResult = systemIndexesIndexService.findByExample({"document_type":{"name": documentTypeName}})
+        if customIndexesResult:
+            if "data" in customIndexesResult:
+                logger.info("Found {} for document {}".format(len(customIndexesResult["data"]),documentTypeName))
+                if len(customIndexesResult["data"])>0:
+                    customIndexes= ",".join(map(lambda i: i["entity"]["name"]+"#"+i["name"], customIndexesResult["data"]))
+        if customIndexes:
+            logger.info("Custom indexes string {}".format(customIndexes))
+            customMatchingIndexes = indexUtils.findIndexFromEntity(customIndexes,newRecord,sk)
+            for i in customMatchingIndexes.keys():
+                try:
+                    logger.info("index found {}".format(i))
+                    index = customMatchingIndexes[i]
+                    logger.info("index tablePrefix {} and conditions {}".format(index["tablePrefix"], index["conditions"]))
+                    indexKeys = index["conditions"]
+                    logger.info("Index keys {}".format(indexKeys))
+                    orderingKey = index["orderBy"] if "orderBy" in index else None
+                    logger.info("Document {} idKey {} orderingKey {}".format(documentTypeName,idKey, orderingKey))
+                    repository =  IndexRepository(tableName,index["tablePrefix"],idKey,orderingKey,indexKeys,dynamoDB=dynamodb)
+                    repositoryLambda(repository,idKey)
+                except Exception as e:
+                    logger.warning("Unable to create the index {}".format(i),exc_info=e)
+
+        logger.info("System indexes string {}".format(indexes))
+        systemMatchingIndexes = indexUtils.findIndexFromEntity(indexes,newRecord,sk)
+        logger.info("Found {} indexes matching the record for system indexes".format(len(systemMatchingIndexes)))
+        for i in systemMatchingIndexes.keys():
+            try:
+                logger.info("index found {}".format(i))
+                index = systemMatchingIndexes[i]
+                logger.info("index tablePrefix {} and conditions {}".format(index["tablePrefix"], index["conditions"]))
+                indexKeys = index["conditions"]
+                logger.info("Index keys {}".format(indexKeys))
+                orderingKey = index["orderBy"] if "orderBy" in index else None
+                logger.info("Entity {} idKey {} orderingKey {}".format(documentTypeName,idKey, orderingKey))
+                repository =  IndexRepository(tableName,index["tablePrefix"],idKey,orderingKey,indexKeys,dynamoDB=dynamodb)
+                repositoryLambda(repository,idKey)
+            except Exception as e:
+                logger.warning("Unable to create the index {}".format(i),exc_info=e)
+    else:
+        logger.info("Unable to retrieve the document_type {} ".format(sk))
