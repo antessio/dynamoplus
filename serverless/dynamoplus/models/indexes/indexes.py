9e02995ffbc0385bdb1f7b1f90660c2041058636
antessio <antessio7@gmail.com>
2019-10-04 00:37:01 +0200

    Query data model

--- serverless/dynamoplus/models/indexes/indexes.py
+++ serverless/dynamoplus/models/indexes/indexes.py
@@ -16,4 +16,10 @@ class Index(object):
     def indexName(self):
         return "__".join(self.conditions)+"__ORDER_BY__"+self.orderingKey
 
+class Query(object):
+    def __init__(self, document:dict,index:Index, startFrom:str=None, limit:int=50):
+        self.document = document
+        self.index = index
+        self.startFrom = startFrom
+        self.limit = limit
     
