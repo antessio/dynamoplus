266f78f490c215b8096145db0bb6f5c84fe18c9c
antessio <antessio7@gmail.com>
2019-10-04 19:40:46 +0200

    Fixed issue with timezone

--- serverless/test/dynamoplus/utils/test_utils.py
+++ serverless/test/dynamoplus/utils/test_utils.py
@@ -3,13 +3,12 @@ import collections
 from collections.abc import Iterable, Mapping, ByteString, Set
 import numbers
 import decimal
-from datetime import datetime
-
+from datetime import datetime,timezone
 from dynamoplus.utils.utils import convertToString, findValue, getValuesByKeyRecursive
 
 class TestUtils(unittest.TestCase):
     def test_convertDateTimeToString(self):
-        self.assertEqual(convertToString(datetime(2019,12,12,8,8,10,100),tzinfo=timezone.utc), "1576134490.0000998973846435546875")
+        self.assertEqual(convertToString(datetime(2019,12,12,8,8,10,100,tzinfo=timezone.utc)), "1576138090.0000998973846435546875")
     def test_convertDecimaToString(self):
         self.assertEqual(convertToString(decimal.Decimal('20.00')), "20.00")
     def test_convertBooleanToString(self):
