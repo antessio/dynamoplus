fd5ef8921fa43f593b70b9051fe2bcef6d437725
antessio <antessio7@gmail.com>
2019-10-05 12:00:45 +0200

    Code cleanup

--- serverless/test/dynamoplus/service/test_configurations.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from  dynamoplus.service.configurations import ConfigurationService
-
-class TestConfigurationService(unittest.TestCase):
-    def setUp(self):
-        pass
-    def test_systemDocumentConfiguration(self):
-        self.assertTrue(True)
