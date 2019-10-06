c5c08fa83af12eccf24217c51b9bc474b70e520f
Antonio D'Alessio <antonio.dalessio@satispay.com>
2019-10-06 18:40:18 +0200

    Cleanup dashboard with next js

--- dashboard/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-const sls = require('serverless-http')
-const binaryMimeTypes = require('./binaryMimeTypes')
-
-const server = require('./server')
-module.exports.server = sls(server, {
-  binary: binaryMimeTypes
-})
