8ff2c6c516e0794c143f74a2eb0f0688ac6e0f0d
Antonio D'Alessio <antonio.dalessio@satispay.com>
2019-10-07 21:02:05 +0200

    Dashboard as lambda with nextjs

---
+++ dashboard/components/meta.js
@@ -0,0 +1,12 @@
+// components/meta.js
+import Head from 'next/head'
+export default ({ props = { title, description } }) => (
+  <div>
+    <Head>
+      <title>{ props.title || 'Next.js Test Title' }</title>
+      <meta name='description' content={props.description || 'Next.js Test Description'} />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta charSet='utf-8' />
+    </Head>
+  </div>
+)
