870566130f4fac95d2f7541ffdd290e49e1b987b
antessio <antessio7@gmail.com>
2019-10-07 21:02:05 +0200

    Dashboard as lambda with nextjs

---
+++ dashboard/layouts/default.js
@@ -0,0 +1,11 @@
+// layouts/default.js
+import React from 'react'
+import Meta from '../components/meta'
+import Navbar from '../components/navbar'
+export default ({ children, meta }) => (
+  <div>
+    <Meta props={meta} />
+    <Navbar />
+    { children }
+  </div>
+)
