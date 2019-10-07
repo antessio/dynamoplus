646d60c11e9fd7a877e076802edbfe9b3b359055
Antonio D'Alessio <antonio.dalessio@satispay.com>
2019-10-07 21:24:52 +0200

    Ant Design

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -8,31 +8,13 @@ class IndexPage extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      loading: true,
-      dog: {}
+        }
     }
-    this.fetchData = this.fetchData.bind(this)
-  }
-  async componentDidMount () {
-    await this.fetchData()
-  }
-  async fetchData () {
-    this.setState({ loading: true })
-    const { data } = await axios.get(
-      'https://api.thedogapi.com/v1/images/search?limit=1'
-    )
-    this.setState({
-      dog: data[0],
-      loading: false
-    })
-  }
   render () {
     return (
       <Default meta={meta}>
         <div>
-          <h1>This is the Front Page.</h1>
-          <h3>Random dog of the day:</h3>
-          <img src={this.state.dog.url} alt='' />
+          <h1>Dynamoplus dashboard</h1>
         </div>
       </Default>
     )
