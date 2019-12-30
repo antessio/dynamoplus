2b84eb289c5e56c431508c49a36601e16b0bf6c6
Antonio D'Alessio <antonio.dalessio@satispay.com>
2019-12-30 18:55:33 +0100

    http signature

--- serverless/dynamoplus/http/authorizer.py
+++ serverless/dynamoplus/http/authorizer.py
@@ -1,27 +1,35 @@
 import os
-from base64 import b64decode, b64encode
+from dynamoplus.service.authorization.authorization import AuthorizationService
 
+AUTHORIZATION_FEATURE_ENABLED = bool(os.getenvb("AUTHORIZATION_FEATURE_ENABLED","False"))
 ROOT_ACCOUNT = os.getenv("ROOT_ACCOUNT")
 ROOT_PASSWORD = os.getenv("ROOT_PASSWORD")
 
 
 def authorize(event, context):
-    whole_auth_token = event["headers"]["Authorization"]
-    try:
-        split = whole_auth_token.split(' ')
-        if len(split) != 2 or split[0].strip().lower() != 'basic':
-            raise Exception("unauthorized")
+    headers = event["headers"]
+    http_method = event["httpMethod"]
+    path = event["path"]
+    method_arn = event["methodArn"]
+    if AUTHORIZATION_FEATURE_ENABLED:
         try:
-            username, password = b64decode(split[1]).decode().split(':', 1)
-            if username == ROOT_ACCOUNT and password == ROOT_PASSWORD:
-                return generate_policy(username, "Allow", "*")
+            if AuthorizationService.is_basic_auth(headers):
+                username = AuthorizationService.is_basic_auth_authorized()
+                if username:
+                    return generate_policy(username, "Allow", "*")
+            elif AuthorizationService.is_api_key(headers):
+                client_authorization = AuthorizationService.get_client_authorization_by_api_key(headers)
+                if AuthorizationService.check_scope(path,http_method,client_authorization.client_scopes):
+                    return generate_policy(client_authorization.client_id, "Allow", "*")
+            elif AuthorizationService.is_http_signature(headers):
+                client_authorization = AuthorizationService.get_client_authorization_using_http_signature_authorized(headers, http_method,path)
+                if AuthorizationService.check_scope(path, http_method, client_authorization.client_scopes):
+                    return generate_policy(client_authorization.client_id, "Allow", "*")
         except Exception as e:
             print(f'Exception encountered: {e}')
             raise Exception('Unauthorized')
-
-    except Exception as e:
-        print(f'Exception encountered: {e}')
-        raise Exception('Unauthorized')
+    else:
+        generate_policy("anonymous_client", "Allow", "*")
 
 
 def generate_policy(principal_id, effect, resource):
