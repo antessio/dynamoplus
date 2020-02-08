5ac6f3f53f90f2d334e74a452173b6d76755ae6c
Antonio D'Alessio <antonio.dalessio@satispay.com>
2020-02-08 23:45:14 +0100

    fixed issue when credentials didn't match

--- serverless/dynamoplus/http/authorizer.py
+++ serverless/dynamoplus/http/authorizer.py
@@ -1,5 +1,8 @@
 import os
+import logging
 from dynamoplus.service.authorization.authorization import AuthorizationService
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 AUTHORIZATION_FEATURE_ENABLED = True # bool(os.getenvb("AUTHORIZATION_FEATURE_ENABLED","False"))
 ROOT_ACCOUNT = os.getenv("ROOT_ACCOUNT")
@@ -11,25 +14,29 @@ def authorize(event, context):
     http_method = event["httpMethod"]
     path = event["path"]
     method_arn = event["methodArn"]
+    policy = generate_policy("anonymous_client","Deny","*")
     if AUTHORIZATION_FEATURE_ENABLED:
         try:
             if AuthorizationService.is_basic_auth(headers):
                 username = AuthorizationService.get_basic_auth_authorized(headers)
+                logging.info("Found {} in credentials".format(username))
                 if username:
-                    return generate_policy(username, "Allow", "*")
+                    policy = generate_policy(username, "Allow", "*")
             elif AuthorizationService.is_api_key(headers):
                 client_authorization = AuthorizationService.get_client_authorization_by_api_key(headers)
                 if AuthorizationService.check_scope(path,http_method,client_authorization.client_scopes):
-                    return generate_policy(client_authorization.client_id, "Allow", "*")
+                    policy = generate_policy(client_authorization.client_id, "Allow", "*")
             elif AuthorizationService.is_http_signature(headers):
                 client_authorization = AuthorizationService.get_client_authorization_using_http_signature_authorized(headers, http_method,path)
                 if AuthorizationService.check_scope(path, http_method, client_authorization.client_scopes):
-                    return generate_policy(client_authorization.client_id, "Allow", "*")
+                    policy = generate_policy(client_authorization.client_id, "Allow", "*")
         except Exception as e:
             print(f'Exception encountered: {e}')
             raise Exception('Unauthorized')
     else:
-        generate_policy("anonymous_client", "Allow", "*")
+        policy = generate_policy("anonymous_client", "Allow", "*")
+
+    return policy
 
 
 def generate_policy(principal_id, effect, resource):
