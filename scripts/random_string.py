ec2b7313dace37a33712aca0f1c1dd6f5395f40f
Antonio D'Alessio <antonio.dalessio@satispay.com>
2020-03-14 12:46:16 +0100

    running dynamoplus locally and test it

---
+++ scripts/random_string.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+print(random_string_generator())
