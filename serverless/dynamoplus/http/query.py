a9215a1a5ad315a730889befbe947813444d7947
Antonio D'Alessio <antonio.dalessio@satispay.com>
2020-03-18 20:14:05 +0100

    better lambda organization

--- serverless/dynamoplus/http/query.py
+++
@@ -1,8 +0,0 @@
-import os
-import logging
-logging.basicConfig(level=logging.INFO)
-from dynamoplus.http.handler.handler import HttpHandler
-
-def query(event, context):
-    handler = HttpHandler()
-    return handler.query(event['pathParameters'], query_string_parameters=event['queryStringParameters'], body=event["body"], headers=event['headers'])
