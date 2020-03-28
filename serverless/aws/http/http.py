a97c6ecac396f85b5007cd458a54e2ffaa33256f
antessio <antessio7@gmail.com>
2020-03-28 22:18:50 +0100

    fixed refactoring typo

--- serverless/aws/http/http.py
+++ serverless/aws/http/http.py
@@ -1,5 +1,6 @@
 # from jsonschema import ValidationError,SchemaError
 from aws.http.handler.handler import HttpHandler
+from dynamoplus.dynamo_plus import get as dynamoplus_get,update as dynamoplus_update,query as dynamoplus_query,create as dynamoplus_create,delete as dynamoplus_delete
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -7,21 +8,21 @@ handler = HttpHandler()
 
 
 def get(event, context):
-    return get(event['pathParameters'])
+    return dynamoplus_get(event['pathParameters'])
 
 
 def query(event, context):
-    return query(event['pathParameters'], query_string_parameters=event['queryStringParameters'],
+    return dynamoplus_query(event['pathParameters'], query_string_parameters=event['queryStringParameters'],
                          body=event["body"], headers=event['headers'])
 
 
 def create(event, context):
-    return create(event["pathParameters"], body=event["body"])
+    return dynamoplus_create(event["pathParameters"], body=event["body"])
 
 
 def update(event, context):
-    return update(event["pathParameters"], body=event["body"])
+    return dynamoplus_update(event["pathParameters"], body=event["body"])
 
 
 def delete(event, context):
-    return handler.delete(event["pathParameters"])
+    return dynamoplus_delete(event["pathParameters"])
