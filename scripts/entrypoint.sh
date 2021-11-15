1fee4dc114b14dd4d144c613234b8448a09f9921
antessio <antessio7@gmail.com>
2021-11-15 23:23:30 +0100

    updated serverless framework

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,5 +3,4 @@
 
 cd /app/dynamoplus/serverless
 export PYTHONPATH=/app/:$PYTHONPATH
-#sls --stage local dynamodb migrate --port=8000 --host=dynamolocal
-INTEGRATION_TEST_FLAG=true sls offline --stage=local
+INTEGRATION_TEST_FLAG=true sls offline --stage=local --noPrependStageInUrl
