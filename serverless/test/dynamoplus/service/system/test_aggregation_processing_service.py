d0679902901a6e20933559beb7d22210aa9384a9
Antonio D'Alessio <antonio.dalessio@satispay.com>
2021-11-27 12:40:01 +0100

    added unit tests

--- serverless/test/dynamoplus/service/system/test_aggregation_processing_service.py
+++ serverless/test/dynamoplus/service/system/test_aggregation_processing_service.py
@@ -3,8 +3,9 @@ import unittest
 from decimal import Decimal
 
 from dynamoplus.models.query.conditions import Eq, And
-from dynamoplus.models.system.aggregation.aggregation import AggregationConfiguration, AggregationType, AggregationTrigger, \
-    AggregationJoin
+from dynamoplus.models.system.aggregation.aggregation import AggregationConfiguration, AggregationType, \
+    AggregationTrigger, \
+    AggregationJoin, Aggregation, AggregationCount, AggregationAvg, AggregationSum
 from dynamoplus.models.system.collection.collection import Collection
 
 from mock import call
@@ -12,6 +13,7 @@ from unittest.mock import patch
 
 from dynamoplus.v2.repository.repositories import Model, Repository, QueryResult, AtomicIncrement, Counter
 from dynamoplus.v2.service.system.aggregation_service import AggregationProcessingService
+from dynamoplus.v2.service.system.system_service import AggregationService, AggregationConfigurationService
 
 domain_table_name = "domain"
 system_table_name = "system"
@@ -19,43 +21,169 @@ system_table_name = "system"
 
 class TestAggregationProcessingService(unittest.TestCase):
 
+
+
+
     def setUp(self):
         os.environ["DYNAMODB_DOMAIN_TABLE"] = domain_table_name
         os.environ["DYNAMODB_SYSTEM_TABLE"] = system_table_name
 
-    @unittest.skip("reason for skipping")
-    @patch.object(Repository, "increment_counter")
-    @patch.object(Repository, "__init__")
-    def test_collection_count_aggregation(self, mock_repository, mock_increment_counter):
-        mock_repository.return_value = None
-        aggregation = AggregationConfiguration("example", AggregationType.COLLECTION_COUNT,
-                                               [AggregationTrigger.INSERT, AggregationTrigger.DELETE], None, None, None)
+    @patch.object(AggregationService, "create_aggregation")
+    @patch.object(AggregationService, "get_aggregation_by_name")
+    def test_collection_count_aggregation_not_found(self, mock_get_aggregation_by_name, mock_createAggregation):
+        aggregation_configuration = AggregationConfiguration("example", AggregationType.COLLECTION_COUNT,
+                                                             [AggregationTrigger.INSERT, AggregationTrigger.DELETE],
+                                                             None, None, None)
+        expected_aggregation = AggregationCount(aggregation_configuration.name, aggregation_configuration.name,1)
         document = {
             "name": "whatever"
         }
         example_collection = Collection("example", "id")
-        AggregationProcessingService.execute_aggregation(aggregation, example_collection, document, None)
-        self.assertTrue(mock_repository.called)
-        self.assertTrue(mock_increment_counter.called)
-        self.assertEqual(call(AtomicIncrement("collection#example", "collection", [Counter("count", Decimal(1))])),
-                         mock_increment_counter.call_args_list[0])
+        mock_get_aggregation_by_name.return_value = None
+        mock_createAggregation.return_value = expected_aggregation
+        result = AggregationProcessingService.execute_aggregation(aggregation_configuration, example_collection, document,
+                                                         None)
+        self.assertEqual(result, expected_aggregation)
+        self.assertEqual(call(aggregation_configuration.name), mock_get_aggregation_by_name.call_args_list[0])
+        self.assertEqual(call(AggregationCount(aggregation_configuration.name, aggregation_configuration.name, 1)),
+                         mock_createAggregation.call_args_list[0])
 
-    @unittest.skip("reason for skipping")
-    @patch.object(Repository, "increment_counter")
-    @patch.object(Repository, "__init__")
-    def test_collection_count_aggregation_remove(self, mock_repository, mock_increment_counter):
-        mock_repository.return_value = None
-        aggregation = AggregationConfiguration("example", AggregationType.COLLECTION_COUNT,
-                                               [AggregationTrigger.INSERT, AggregationTrigger.DELETE], None, None, None)
+    @patch.object(AggregationService, "increment_count")
+    @patch.object(AggregationService, "get_aggregation_by_name")
+    def test_collection_count_aggregation_increment(self, mock_get_aggregation_by_name, mock_increment_count):
+
+        aggregation_configuration = AggregationConfiguration("example", AggregationType.COLLECTION_COUNT,
+                                                             [AggregationTrigger.INSERT, AggregationTrigger.DELETE],
+                                                             None, None, None)
         document = {
             "name": "whatever"
         }
         example_collection = Collection("example", "id")
-        AggregationProcessingService.execute_aggregation(aggregation, example_collection, None, document)
-        self.assertTrue(mock_repository.called)
-        self.assertEqual(
-            call(AtomicIncrement("collection#example", "collection", [Counter("count", Decimal(1), False)])),
-            mock_increment_counter.call_args_list[0])
+        expected_existing_aggregation_count = AggregationCount(aggregation_configuration.name, aggregation_configuration.name, 1)
+        mock_get_aggregation_by_name.return_value = expected_existing_aggregation_count
+        mock_increment_count.return_value = expected_existing_aggregation_count
+        existing_count = expected_existing_aggregation_count.count
+        result = AggregationProcessingService.execute_aggregation(aggregation_configuration, example_collection, document,
+                                                         None)
+
+        self.assertEqual(result.count, existing_count + 1)
+        self.assertEqual(call(aggregation_configuration.name), mock_get_aggregation_by_name.call_args_list[0])
+        self.assertEqual(call(expected_existing_aggregation_count),
+                         mock_increment_count.call_args_list[0])
+
+    @patch.object(AggregationService, "decrement_count")
+    @patch.object(AggregationService, "get_aggregation_by_name")
+    def test_collection_count_aggregation_decrement(self, mock_get_aggregation_by_name, mock_decrement_count):
+        aggregation_configuration = AggregationConfiguration("example", AggregationType.COLLECTION_COUNT,
+                                                             [AggregationTrigger.INSERT, AggregationTrigger.DELETE],
+                                                             None, None, None)
+        document = {
+            "name": "whatever"
+        }
+        example_collection = Collection("example", "id")
+        expected_existing_aggregation_count = AggregationCount(aggregation_configuration.name,
+                                                               aggregation_configuration.name, 10)
+        expected_existing_count = expected_existing_aggregation_count.count
+        mock_get_aggregation_by_name.return_value = expected_existing_aggregation_count
+        mock_decrement_count.return_value = expected_existing_aggregation_count
+        result = AggregationProcessingService.execute_aggregation(aggregation_configuration, example_collection,
+                                                                  None,
+                                                                  document)
+        self.assertEqual(result.count, expected_existing_count-1)
+        self.assertEqual(call(aggregation_configuration.name), mock_get_aggregation_by_name.call_args_list[0])
+        self.assertEqual(call(expected_existing_aggregation_count),
+                         mock_decrement_count.call_args_list[0])
+
+    @patch.object(AggregationService, "create_aggregation")
+    @patch.object(AggregationService, "get_aggregation_by_name")
+    @patch.object(AggregationService, "get_aggregations_by_name_generator")
+    def test_collection_avg_aggregation_not_found(self,mock_get_aggregations_by_name_generator, mock_get_aggregation_by_name, mock_createAggregation):
+        target_field = "attribute"
+        aggregation_configuration = AggregationConfiguration("example", AggregationType.AVG,
+                                                             [AggregationTrigger.INSERT, AggregationTrigger.UPDATE, AggregationTrigger.DELETE],
+                                                             target_field, None, None)
+        expected_value = 7.0
+        expected_aggregation = AggregationAvg(aggregation_configuration.name, aggregation_configuration.name, expected_value)
+        document = {
+            "name": "whatever",
+            target_field: expected_value
+        }
+        example_collection = Collection("example", "id")
+        mock_get_aggregations_by_name_generator.return_value = []
+        mock_get_aggregation_by_name.return_value = None
+        mock_createAggregation.return_value = expected_aggregation
+        result = AggregationProcessingService.execute_aggregation(aggregation_configuration, example_collection,
+                                                                  document,
+                                                                  None)
+        self.assertEqual(result, expected_aggregation)
+        mock_get_aggregation_by_name.assert_not_called()
+        mock_createAggregation.assert_has_calls([
+            call(AggregationCount("count_" + aggregation_configuration.name, aggregation_configuration.name, 1)),
+            call(AggregationSum("sum_" + aggregation_configuration.name, aggregation_configuration.name, expected_value)),
+            call(AggregationAvg(aggregation_configuration.name, aggregation_configuration.name, expected_value))
+        ])
+
+    @patch.object(AggregationService, "increment")
+    @patch.object(AggregationService, "updateAggregation")
+    @patch.object(AggregationService, "get_aggregation_by_name")
+    @patch.object(AggregationService, "get_aggregations_by_name_generator")
+    def test_collection_avg_aggregation_increae(self, mock_get_aggregations_by_name_generator,
+                                                mock_get_aggregation_by_name, mock_update_aggregation, mock_increment):
+        target_field = "attribute"
+        aggregation_configuration = AggregationConfiguration("example", AggregationType.AVG,
+                                                             [AggregationTrigger.INSERT, AggregationTrigger.UPDATE,
+                                                              AggregationTrigger.DELETE],
+                                                             target_field, None, None)
+        existing_avg = 7.0
+        existing_sum = 14
+        existing_count = 2
+        value = 3
+        expected_new_count = existing_count + 1
+        expected_new_sum = existing_sum + value
+        expected_new_avg = (expected_new_sum) / (expected_new_count)
+        expected_aggregation = AggregationAvg(aggregation_configuration.name, aggregation_configuration.name,
+                                              expected_new_avg)
+        new_record = {
+            "name": "whatever",
+            target_field: value
+        }
+        example_collection = Collection("example", "id")
+        expected_existing_aggregtion_count = AggregationCount("count_" + aggregation_configuration.name, aggregation_configuration.name, existing_count)
+        expected_existing_aggregation_sum = AggregationSum("sum_" + aggregation_configuration.name, aggregation_configuration.name, existing_sum)
+        mock_get_aggregations_by_name_generator.return_value = [
+            AggregationAvg(aggregation_configuration.name, aggregation_configuration.name,
+                           existing_avg),
+            expected_existing_aggregation_sum,
+            expected_existing_aggregtion_count
+        ]
+        mock_get_aggregation_by_name.side_effect = [
+            expected_existing_aggregtion_count,
+            expected_existing_aggregation_sum,
+            AggregationAvg(aggregation_configuration.name, aggregation_configuration.name,
+                           existing_avg)
+        ]
+        mock_increment.side_effect = [
+            AggregationCount("count_" + aggregation_configuration.name, aggregation_configuration.name, expected_new_sum-1),
+            AggregationSum("sum_" + aggregation_configuration.name, aggregation_configuration.name, expected_new_sum)
+        ]
+        mock_update_aggregation.return_value = AggregationAvg(aggregation_configuration.name, aggregation_configuration.name, expected_new_avg)
+
+        result = AggregationProcessingService.execute_aggregation(aggregation_configuration, example_collection,
+                                                                  new_record,
+                                                                  None)
+        self.assertEqual(result, expected_aggregation)
+        mock_get_aggregation_by_name.assert_has_calls([
+            call('example_avg_attribute'),
+            call('sum_example_avg_attribute'),
+            call('count_example_avg_attribute')
+        ])
+        mock_update_aggregation.assert_has_calls([
+            call(AggregationAvg(aggregation_configuration.name, aggregation_configuration.name, expected_new_avg))
+        ])
+        mock_increment.assert_has_calls([
+            call(expected_existing_aggregtion_count,"count",1),
+            call(expected_existing_aggregation_sum, "sum", value),
+        ])
 
     @unittest.skip("reason for skipping")
     @patch.object(Repository, "increment_counter")
